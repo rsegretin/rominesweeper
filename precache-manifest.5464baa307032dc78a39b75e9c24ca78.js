98ca7389c17d7f242724f637a940e7c68773c1b3
rsegretin <rsegretin@yahoo.com.ar>
2020-11-02 18:11:30 -0300

    Updates

---
+++ precache-manifest.5464baa307032dc78a39b75e9c24ca78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c965bdfd97df3f13e6dac8a8b34a85",
+    "url": "/rominesweeper/index.html"
+  },
+  {
+    "revision": "f74d777fe4474be93417",
+    "url": "/rominesweeper/static/css/main.b2d12f8f.chunk.css"
+  },
+  {
+    "revision": "d0359073114b5a756646",
+    "url": "/rominesweeper/static/js/2.3c886981.chunk.js"
+  },
+  {
+    "revision": "58d64a5bc3b06f2933294a2521985f38",
+    "url": "/rominesweeper/static/js/2.3c886981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74d777fe4474be93417",
+    "url": "/rominesweeper/static/js/main.3632b7d0.chunk.js"
+  },
+  {
+    "revision": "56d1b2d6346866d3237f",
+    "url": "/rominesweeper/static/js/runtime-main.e7935655.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rominesweeper/precache-manifest.5464baa307032dc78a39b75e9c24ca78.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rominesweeper/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
